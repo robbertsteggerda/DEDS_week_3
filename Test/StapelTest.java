30ab85426351dfb82d3644e87691f3a40dc43a07
robbertsteggerda <20216092@student.hhs.nl>
2023-02-20 15:03:00 +0100

    help

--- Test/StapelTest.java
+++ Test/StapelTest.java
@@ -1,13 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
-        testDuw();
-    }
-    public static void testDuw() {
+import org.junit.jupiter.api.*;
+
+public class StapelTest {
+    @Test
+    public void testDuw() {
         Stapel stapel = new Stapel();
         stapel.duw(1).duw(3).duw("bob");
         for (int i = 0; i < 5; i++) {
             System.out.print(stapel.pak() + ", ");
         }
     }
+    @Test
+    public void testPak() {
 
+    }
 }
--- src/Stapel.java
+++ src/Stapel.java
@@ -5,20 +5,20 @@ public class Stapel<T>{
         private T waarde;
         private StapelKnoop volgende;
 
-        private StapelKnoop(T waarde, StapelKnoop volgende) {
-            waarde = this.waarde;
-            volgende = this.volgende;
+        private StapelKnoop(T waarde,StapelKnoop volgende){
+            this.waarde = waarde;
+            this.volgende = volgende;
         }
     }
-    private Stapel<T> vorige;
-    private T waarde;
 
-    public Stapel<T> duw(T waarde) {
-        bovenkant = new StapelKnoop(waarde, bovenkant);
+    public Stapel<T> duw(T w) {
+        bovenkant = new StapelKnoop(w, bovenkant);
         return this;
     }
-
     public T pak() {
+        if (bovenkant.waarde == null){
+            throw new IllegalArgumentException("stapel is leeg");
+        }
         T data = bovenkant.waarde;
         bovenkant = bovenkant.volgende;
         return data;
