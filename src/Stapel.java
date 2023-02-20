372df09fdd18b95e00cf45a88e0e146b05381010
robbertsteggerda <20216092@student.hhs.nl>
2023-02-20 13:14:44 +0100

    Stapel herschreven om een Nested Class te gebruiken

--- src/Stapel.java
+++ src/Stapel.java
@@ -1,30 +1,17 @@
 public class Stapel<T>{
+
+    private StapelKnoop bovenkant;
+    private class StapelKnoop {
+
+        private T waarde;
+        private StapelKnoop volgende;
+
+        private StapelKnoop(T waarde, StapelKnoop volgende) {
+            waarde = waarde;
+            volgende = volgende;
+        }
+    }
+
     private Stapel<T> vorige;
     private T waarde;
-    
-    Stapel(Stapel<T> vorige,T waarde){
-        this.vorige = vorige;
-        this.waarde = waarde;
-    }
-
-    public void duw(T waarde){
-        this.vorige = new Stapel<T>(this.vorige,this.waarde);
-        this.waarde = waarde;
-    }
-
-    public T pak(){
-        if(this.isLeeg()){
-            throw new IllegalArgumentException("Stapel is leeg");
-        }
-
-        T bovenkant = this.waarde;
-        this.waarde = vorige.waarde;
-        this.vorige = this.vorige.vorige;
-
-        return bovenkant;
-    }
-
-    private boolean isLeeg() {
-        return this.vorige == null;
-    }
 }
